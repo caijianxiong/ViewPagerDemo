7f3c740dcdf920fbc1fa0f5faedb714e4b3aec5b
cjx <1153448695@qq.com>
2019-10-29 09:19:43 +0800

    update .ignore

--- app/src/main/java/cjx/liyueyun/viewpagerdemo/adapter/MyPagerAdapter.java
+++ app/src/main/java/cjx/liyueyun/viewpagerdemo/adapter/MyPagerAdapter.java
@@ -3,7 +3,6 @@ package cjx.liyueyun.viewpagerdemo.adapter;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.TextView;
 
 import java.util.List;
